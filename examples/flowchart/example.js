6c2f991ba28068ab5e9fadef62d36fa6866b6851
Sean Sullivan <ssullivan@novadynamics.com>
2022-12-23 14:14:09 -0800

    Added FlowChart

---
+++ examples/flowchart/example.js
@@ -0,0 +1,48 @@
+const { DataState, Window, Textbox, Element } = require("../../index.js")
+
+window.jQuery = window.$ = require("jquery");
+window.$ = require("jquery");
+
+var w = new Window(window.$("#container"), {});
+
+
+var a = w.add_subplot({top:1,bottom:2,left:1,right:2},{
+  width : 600,
+  height : 400,
+  margins : {
+    top: 30, right: 20, bottom: 30, left: 50
+  },
+  xlim : [-5,5],
+  ylim : [-5,5],
+  xlabel : "x",
+  ylabel : "y"
+});
+
+let chart_ds = new DataState.FlowChart()
+
+chart_ds.nodes = [
+  {title: "quotation", width: 100, height: 50},
+  {title: "process order", width: 100, height: 50},
+  {title: "production", width: 100, height: 50, active: true},
+  {title: "Purchasing", width: 100, height: 50},
+  {title: "Delivery", width: 100, height: 50}
+]
+
+chart_ds.links = [
+  {source: 0, target: 1, link_label: "yourmom"},
+  {source: 1, target: 2, link_label: "yourmom"},
+  {source: 1, target: 3, link_label: "yourmom"},
+  {source: 3, target: 2, link_label: "yourmom"},
+  {source: 2, target: 4, link_label: "yourmom"},
+]
+
+a.add_element(new Element.FlowChart(chart_ds, {fcolor: "#ff9100"}));
+
+w.init();
+w.start();
+
+setInterval(()=>{
+  chart_ds.nodes.forEach((n)=>n.active = false)
+
+  chart_ds.is_updated = true
+}, 500)
